bdec75e4fed8ec24b16325c18515e2ffc9b35f36
Oliver Edman Chrantz <o.edman@icloud.com>
2026-06-14 22:40:05 +0200

    Refactor circular_buffer_t to struct circular_buffer

--- include/circular_buffer.h
+++ include/circular_buffer.h
@@ -6,27 +6,27 @@
 #include <stddef.h>
 
 
-typedef struct {
-    uint16_t *buffer;   // Pointer where the array data is stored
-    size_t size;        // Total number of elements in the buffer
-    size_t head;        // Index for the next write operation (push)
-    size_t tail;        // Index for the next read operation (pop)
-    bool is_full;       // Set to true when the buffer is full
-} circular_buffer_t;
+struct circular_buffer {
+	uint16_t *buffer;	// Pointer where the array data is stored
+	size_t size;		// Total number of elements in the buffer
+	size_t head;		// Index for the next write operation (push)
+	size_t tail;		// Index for the next read operation (pop)
+	bool is_full;		// Set to true when the buffer is full
+};
 
 
-void cb_init(circular_buffer_t *cb, uint16_t *buffer, size_t size);
+void cb_init(struct circular_buffer *cb, uint16_t *buffer, size_t size);
 
 
-bool cb_push(circular_buffer_t *cb, uint16_t data);
+bool cb_push(struct circular_buffer *cb, uint16_t data);
 
 
-bool cb_pop(circular_buffer_t *cb, uint16_t *data);
+bool cb_pop(struct circular_buffer *cb, uint16_t *data);
 
 
-bool cb_is_empty(circular_buffer_t *cb);
+bool cb_is_empty(struct circular_buffer *cb);
 
 
-bool cb_is_full(circular_buffer_t *cb);
+bool cb_is_full(struct circular_buffer *cb);
 
 #endif // CIRCULAR_BUFFER_H
